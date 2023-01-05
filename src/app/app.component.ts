299b636d2bb92f25620684556d827e05ca002d6e
PriteshKiri <pritesh.d.kiri@gmail.com>
2023-01-05 18:17:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WheatherService } from './services/wheather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+  constructor(private weatherSevices: WheatherService) {}
+  weatherData?: WeatherData;
+  cityName: string = 'London';
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+  private getWeatherData(cityName: string) {
+    this.weatherSevices.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      },
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  weatherApiBaseUrl: 'http://api.weatherapi.com/v1/current.json',
+  apiKey: '14d36a8ca00340bd9ca113552230501',
+};
